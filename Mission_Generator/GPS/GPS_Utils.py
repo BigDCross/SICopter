b2b0a7ceded20e889b5e922dbced6ae3c13280de
Dustin Crossman <dustin.crossman@gmail.com>
2015-03-26 14:01:02 -0700

    Mission Generator Working. Currently only N/S and E/W offsets are supported.

--- Mission_Generator/GPS/GPS_Utils.py
+++ Mission_Generator/GPS/GPS_Utils.py
@@ -16,8 +16,13 @@ def haversine(lat1, lon1, lat2, lon2):
     return Earth_Radius * c * 1000
 
 # Returns gps coords based on north/south and east/west offset in METERS
-def calc_gps_offset(lat, lon, dx, dy):
-    new_lat = lat + degrees(dy/(Earth_Radius * 1000.0))
+def calc_gps_offset(lon, lat, dx, dy):
     new_lon = lon + degrees(dx/(Earth_Radius * 1000.0) / cos(radians(lat)))
+    new_lat = lat + degrees(dy/(Earth_Radius * 1000.0))
 
-    return new_lat, new_lon
+    return new_lon, new_lat
+
+#print calc_gps_offset(47, -117, 0, 20)
+#print calc_gps_offset(47, -117, 20, 0)
+#print calc_gps_offset(47, -117, 0, -20)
+#print calc_gps_offset(47, -117, -20, 0)
--- Mission_Generator/Mission_Generator.py
+++ Mission_Generator/Mission_Generator.py
@@ -1,31 +1,34 @@
 from math import radians, sin, cos, sqrt, asin, degrees
 
-import GPS.GPS_Utils
+from GPS.GPS_Utils import haversine, calc_gps_offset
+
+import sys
 
 class GPSCoord:
-    def __init__(self, lat, lon):
-        self.lat = lat
-        self.lon = lon
+    def __init__(self, lon, lat):
+        self.lon = float(lon)
+        self.lat = float(lat)
 
     def __add__(self, other):
-        return GPS_Utils.calc_gps_offset(self.lat, self.lon, other[0], other[1])
+        new_lon, new_lat = calc_gps_offset(self.lon, self.lat, other[0], other[1])
+        return GPSCoord(new_lon, new_lat)
 
     def __iadd__(self, other):
-        self.lat, self.lon = GPS_Utils.calc_gps_offset(self.lat, self.lon, other[0], other[1])
-        return self.lat, self.lon
+        self.lon, self.lat = calc_gps_offset(self.lon, self.lat, other[0], other[1])
+        return GPSCoord(self.lon, self.lat)
 
     def __str__(self):
-        return str("<{0}, {1}>".format(self.lat, self.lon))
+        return str("<{0}, {1}>".format(self.lon, self.lat))
 
     def __repr__(self):
-        return str("<{0}, {1}>".format(self.lat, self.lon))
+        return str("<{0}, {1}>".format(self.lon, self.lat))
 
 class MissionGenerator:
     def __init__(self):
         pass
 
-    def setStartingPos(self, lat, lon):
-        self.starting_point = GPSCoord(lat, lon)
+    def setStartingPos(self, lon, lat):
+        self.starting_point = GPSCoord(lon, lat)
 
     def generate(self):
         pass
@@ -44,10 +47,49 @@ class MissionParser:
 
     def parse(self):
         print "Parsing!"
-        lines = self.mission_file.read()
-        self.mission_outfile.write(lines)
-        print lines
+        lines = self.mission_file.readlines()
 
-parser = MissionParser("Missions/Fly_Square_SI_Mission.txt", "Missions/Banana.txt")
+        for i, curr_line in enumerate(lines):
+            tokens = curr_line.split("\t")
 
-parser.parse()
+            if i == 0:
+                self.mission_outfile.write("QGC WPL 110\n")
+            elif i == 1:
+                curr_line = curr_line[0:-2] # Kill newline at end (this might fail on windows)
+                self.mission_outfile.write(curr_line)
+                self.mission_outfile.write("\n")
+
+                self.starting_point = GPSCoord(tokens[9], tokens[8]) # tokens[8]: lat tokens[9]: lon
+                next_pos = self.starting_point
+            else:
+                dy = float(tokens[8])
+                dx = float(tokens[9])
+                next_pos += (dx, dy)
+
+                for j, tok in enumerate(tokens):
+                    if j == 8:
+                        self.mission_outfile.write(str(next_pos.lat))
+                        self.mission_outfile.write("\t")
+                    elif j == 9:
+                        self.mission_outfile.write(str(next_pos.lon))
+                        self.mission_outfile.write("\t")
+                    elif j == 11: # No tab at end
+                        tok = tok[0:-2] # Kill newline at end
+                        self.mission_outfile.write(tok)
+                        self.mission_outfile.write("\n")
+                    else:
+                        self.mission_outfile.write(tok)
+                        self.mission_outfile.write("\t")
+
+        self.mission_file.close()
+        self.mission_outfile.close()
+
+def main():
+    if len(sys.argv) != 3:
+        print "Usage: python Mission_Generator.py <SI_Mission> <Output_Mission_Name>"
+        sys.exit()
+    else:
+        parser = MissionParser(sys.argv[1], sys.argv[2])
+        parser.parse()
+
+main()
